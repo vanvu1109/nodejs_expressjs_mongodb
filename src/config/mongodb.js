f1bfde4e570aa6833dac9170132d46591743e62d
Van Vu <tuilavanvuday@gmail.com>
2026-05-22 21:12:56 +0700

    create project

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,11 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+//vanvudev
+//password123456
+//mongodb+srv://vanvudev:<db_password>@cluster0.5d0mnsw.mongodb.net/?appName=Cluster0
+
+const MONGODB_URL = 'mongodb+srv://vanvudev:password123456@cluster0.5d0mnsw.mongodb.net/?appName=Cluster0'
+
+const DATABASE_NAME = 'trello-vanvudev-mern-stack-pro'
--- src/server.js
+++ src/server.js
@@ -1,8 +1,3 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
 
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
@@ -14,12 +9,13 @@ const port = 8017
 
 app.get('/', (req, res) => {
   // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
   console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
+    [{ id: 'id-1', name: 'One' },
       { id: 'id-2', name: 'Two' },
       { id: 'id-3', name: 'Three' },
       { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
+      { id: 'id-5', name: 'Five' }],
     ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
     'id'
   ))
@@ -28,5 +24,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  console.log(`Hello Van Vu Dev, I am running at http://${ hostname }:${ port }/`)
 })
